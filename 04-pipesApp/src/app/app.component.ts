5435d443c4eb3961a732e6866c26c0770f29b07a
Brais P. Castro <braispcastro@gmail.com>
2021-03-09 14:54:37 +0100

    Avance curso

---
+++ 04-pipesApp/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'pipesApp';
+
+  nombre: string = 'brais castro';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Brais'
+  }
+
+  mostrarNombre(): void {
+    
+  }
+
+}
