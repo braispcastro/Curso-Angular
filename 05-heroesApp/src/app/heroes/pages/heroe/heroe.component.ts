149f9f2a89678179b37f82e8a8d0522dbaf20460
Brais P. Castro <braispcastro@gmail.com>
2021-03-11 21:15:04 +0100

    Avance curso

--- 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,21 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 5px;
+      }
+    `
   ]
 })
 export class HeroeComponent implements OnInit {
 
-  constructor( private activatedRoute: ActivatedRoute ) { }
+  heroe!: Heroe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     
     this.activatedRoute.params
-      .subscribe( ({id}) => console.log(id) );
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroeById(id) )
+      )
+      .subscribe( heroe => {
+        setTimeout(() =>  this.heroe = heroe, 100)
+      } );
 
+  }
+
+  volver() {
+    this.router.navigate(['/heroes/listado'])
   }
 
 }
