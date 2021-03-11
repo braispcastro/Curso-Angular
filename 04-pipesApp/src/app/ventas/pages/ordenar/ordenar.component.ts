432480d5f4e034bcc9a911182ee76cc38b737dad
Brais P. Castro <braispcastro@gmail.com>
2021-03-11 16:08:59 +0100

    Avance curso

--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
+
+  isUpper: boolean = false;
+  ordenarPor: string = '';
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Barman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  toggleUpper(): void {
+    this.isUpper = !this.isUpper;
+  }
+
+  cambiarOrdenar(valor: string): void {
+    this.ordenarPor = valor;
   }
 
 }
---
+++ 04-pipesApp/src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+
+@Pipe({
+    name: 'mayusculas'
+})
+export class MayusculasPipe implements PipeTransform {
+
+    transform(value: string, toUpper: boolean = true): string {
+
+        return toUpper ? value.toLocaleUpperCase() : value.toLocaleLowerCase();
+
+    }
+
+}
