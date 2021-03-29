c022d900963e2f7aa5acda5d70a5928dd6c66e6f
Brais P. Castro <braispcastro@gmail.com>
2021-03-29 17:27:27 +0200

    Formularios

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
+
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +13,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  get emailErrorMessage(): string {
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.required) {
+      return 'El campo email es obligatorio';
+    }
+    else if (errors?.pattern) {
+      return 'El valor introducido no es un email';
+    }
+    else if (errors?.emailUsado) {
+      return 'El email ya está siendo usado';
+    }
+    else {
+      return '';
+    }
+  }
+  
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
+    username: ['', [Validators.required, this.validatorService.noPuedeSerStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password_repeat: ['', [Validators.required]]
+  }, {
+    validators: [this.validatorService.camposIguales('password', 'password_repeat')]
+  });
+
+
+  constructor(private formBuilder: FormBuilder,
+              private validatorService: ValidatorService,
+              private emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Brais Castro',
+      email: 'test1@test.com',
+      username: 'braiscastro',
+      password: '123456',
+      password_repeat: '123456'
+    });
+  }
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+  }
+
+  emailRequired() {
+    return this.miFormulario.get('email')?.errors?.required && this.miFormulario.get('email')?.touched;
+  }
+
+  emailFormato() {
+    return this.miFormulario.get('email')?.errors?.pattern && this.miFormulario.get('email')?.touched;
+  }
+
+  emailYaUsado() {
+    return this.miFormulario.get('email')?.errors?.emailUsado && this.miFormulario.get('email')?.touched;
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
   }
 
 }
